6b88bef56d6acf448a2ac5e7141c1bce6b793c70
Michael Dayah <michael@dayah.com>
2023-07-29 03:31:08 -0400

    Direct to newsmemory.

--- knoxnews.mjs
+++ knoxnews.mjs
@@ -14,7 +14,7 @@ chromium.use(RecaptchaPlugin(RecaptchaOptions))
 
 chromium.launch({ headless: false }).then(async (browser) => {
   const page = await browser.newPage()
-  await page.goto('https://login.knoxnews.com/PKNS-GUP/authenticate/', { waitUntil: 'networkidle'})
+  await page.goto('https://knoxvillenews-tn.newsmemory.com/', { waitUntil: 'networkidle'})
   await page.getByRole('textbox', { name: 'Account Username' }).click()
   await page.getByRole('textbox', { name: 'Account Username' }).type(process.env.NEWS_LOGIN)
   await page.waitForTimeout(3000)
@@ -26,8 +26,6 @@ chromium.launch({ headless: false }).then(async (browser) => {
 
   await page.solveRecaptchas()
 
-  await page.getByLabel('Hi, Michael').click()
-  await page.getByRole('link', { name: 'eNewspaper' }).nth(1).click()
   await page.frameLocator('iframe[name="mainframe"]').frameLocator('iframe[name="mainframe"]').locator('#lightbox-close').click()
   await page.frameLocator('iframe[name="mainframe"]').frameLocator('iframe[name="mainframe"]').getByRole('link', { name: 'View thumbnail images of all pages in the publication. Download pages as PDF files. Pages' }).click()
   const downloadPromise = page.waitForEvent('download')
