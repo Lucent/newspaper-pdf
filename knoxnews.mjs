7959f5aee9fe6a2dd89da361428632b5c0fbb150
Michael Dayah <michael@dayah.com>
2023-07-29 06:08:01 -0400

    Make generic for all newsmemory.com subdomains.

--- knoxnews.mjs
+++ knoxnews.mjs
@@ -14,14 +14,13 @@ chromium.use(RecaptchaPlugin(RecaptchaOptions))
 
 chromium.launch({ headless: false }).then(async (browser) => {
   const page = await browser.newPage()
-  await page.goto('https://knoxvillenews-tn.newsmemory.com/', { waitUntil: 'networkidle'})
+  await page.goto(`https://${process.env.NEWSMEMORY_SUBDOMAIN}.newsmemory.com/`, { waitUntil: 'networkidle'})
   await page.getByRole('textbox', { name: 'Account Username' }).click()
   await page.getByRole('textbox', { name: 'Account Username' }).type(process.env.NEWS_LOGIN)
-  await page.waitForTimeout(3000)
   await page.getByRole('textbox', { name: 'Account Username' }).blur()
   await page.getByRole('textbox', { name: 'Enter Password' }).click()
   await page.getByRole('textbox', { name: 'Enter Password' }).type(process.env.NEWS_PASSWORD)
-  await page.waitForTimeout(3000)
+  await page.getByRole('textbox', { name: 'Enter Password' }).blur()
   await page.getByRole('button', { name: 'Sign In' }).click()
 
   await page.solveRecaptchas()
@@ -32,6 +31,6 @@ chromium.launch({ headless: false }).then(async (browser) => {
   await page.frameLocator('iframe[name="mainframe"]').frameLocator('iframe[name="mainframe"]').locator('#divph_0 div a').click()
   const download = await downloadPromise
   await download.path()
-  await download.saveAs('A1.pdf');
+  await download.saveAs('A1.pdf')
   await browser.close()
-});
+})
